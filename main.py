08b8a60fc3c3400b48ac70a27c4f33b7e0721a02
Snehasis4321 <snehasisghosh2017@gmail.com>
2023-01-16 20:23:27 +0530

    recover deleted

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import warnings
-from test import predict
+from read_models import predict
 
 warnings.filterwarnings('ignore')
 
